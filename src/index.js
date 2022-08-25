894d1da96159496d6eb2db45ce1042155d750e76
MakrFrost <frostmark@ukr.net>
2022-08-25 16:46:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import gallery from './js/01-gallery';
-import video from './js/02-video';
-import feedback from './js/03-feedback';
+// import gallery from './js/01-gallery';
+// import video from './js/02-video';
+// import feedback from './js/03-feedback';
