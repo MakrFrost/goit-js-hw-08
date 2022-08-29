6402b42bd7a3fdbc589f476157e0dbf15e2170d1
MakrFrost <frostmark@ukr.net>
2022-08-29 12:00:22 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,4 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
-function log() {
-  console.log(galleryItems);
-}
-
-log();
 
 console.log(galleryItems);
