40cf632ffa05b275d6b0ba5070aad61f1b50f525
MakrFrost <frostmark@ukr.net>
2022-08-30 11:53:38 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,17 @@
 import Player from '@vimeo/player';
 
-const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
 player.on('play', function () {
   console.log('played the video!');
 });
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
+player.on('play', function (timeupdate) {
+  console.log(timeupdate);
 });
 
+player.setCurrentTime(timeupdate).then(function (timeupdate) {});
 // const iframe = document.querySelector('iframe');
 // const player = new Vimeo.Player(iframe);
 
