7362d4a47ee9f355dc1ad392ffffff3101a21875
MakrFrost <frostmark@ukr.net>
2022-08-30 12:56:28 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,30 +3,26 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
 
 //!
 player.on('play', function (timeupdate) {
-  const timePause = timeupdate.seconds;
+  let timePause = timeupdate.seconds;
   console.log(timePause);
 
   localStorage.setItem('videoplayer-current-time', timePause);
+});
 
+function onUpdatePage() {
+  let savedTimePlayer = localStorage.getItem('videoplayer-current-time');
   player
-    .setCurrentTime(timePause)
-    .then(function (seconds) {
-      player
-        .play()
-        .then(function () {
-          console.log('the video was played');
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    })
+    .setCurrentTime(savedTimePlayer)
+    .then(function () {})
     .catch(function (error) {
       console.log(error);
     });
-});
+}
+
+onUpdatePage();
