298f507a4e4aa3f86b63c5d8cd13d18fba2e4f76
MakrFrost <frostmark@ukr.net>
2022-08-30 11:08:27 +0300

    Update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 
-console.log(galleryItems);
+const galleryAlbum = document.querySelector('.gallery');
+galleryAlbum.addEventListener('click', openGalleryItem);
+
+// Добавляет картинки из объекта в разметку
+function addItemToGallery({ preview, original, description }) {
+  return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" 
+            src="${preview}"
+            alt="${description}" />
+        </a>`;
+}
+
+// Делаем один бойльшой файл разметки
+let addNewImage = galleryItems.map(addItemToGallery).join('');
+//* console.log(addNewImage);
+
+// Вставляет в разметку разметку картинки
+galleryAlbum.insertAdjacentHTML('afterbegin', addNewImage);
+//* console.log(galleryAlbum);
+
+// Открывает модальное окно с картинкой по клику (большой, оригинальной)
+function openGalleryItem(event) {
+  event.preventDefault();
+  // Достукиваемся до дата-атрибута по клику
+  const imageSrc = event.target.dataset.source;
+  //* console.log(imageSrc);
+  //* console.log(event.target);
+}
+
+let gallery = new SimpleLightbox('.gallery a');
+
+let captionsData = (gallery.options.captionsData = 'alt');
+let captionDelay = (gallery.options.captionDelay = 250);
