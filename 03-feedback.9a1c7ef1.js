089c09a96f731d89b2513f66250306ad4636efed
MakrFrost <101950285+MakrFrost@users.noreply.github.com>
2022-08-31 09:13:45 +0000

    Deploying to gh-pages from @ MakrFrost/goit-js-hw-08@a59c8bef454da77473ddd7a88d768a02239b2037 🚀

---
+++ 03-feedback.9a1c7ef1.js
@@ -0,0 +1,2 @@
+console.log("Это фидбек клайентса!");
+//# sourceMappingURL=03-feedback.9a1c7ef1.js.map
---
+++ 03-feedback.e7ec79a5.js
@@ -0,0 +1,2 @@
+console.log("Это фидбек клайентса!");
+//# sourceMappingURL=03-feedback.e7ec79a5.js.map
