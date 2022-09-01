a67929948dee41debc525a3a88760b45bfd0f5d5
MakrFrost <frostmark@ukr.net>
2022-09-01 13:34:09 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,6 +7,8 @@ feedback();
 const form = document.querySelector('.feedback-form');
 const textareas = document.querySelector('.feedback-form textarea');
 
+const STG_KEY = 'feedback-form-state';
+
 form.addEventListener('submit', formSubmit);
 textareas.addEventListener('input', textAreaInput);
 // email.addEventListener('input', emailInput);
@@ -16,24 +18,22 @@ function formSubmit(event) {
   event.preventDefault();
   console.log('Вы отправили форму вашему менеджеру!');
   event.target.reset();
+  localStorage.removeItem(STG_KEY);
 }
 
 function textAreaInput(event) {
   const messageValue = event.target.value;
   console.log(messageValue);
-  localStorage.setItem('feedback-form-state', messageValue);
+  localStorage.setItem(STG_KEY, messageValue);
 }
 
-function messageInput(event) {
-  // const text = event.target.value;
-  // console.log(text);
-  // localStorage.setItem('feedback-form-state', text);
-}
+function messageInput(event) {}
 
 function onRefreshPage() {
-  const saveMessage = localStorage.getItem('feedback-form-state');
+  const saveMessage = localStorage.getItem(STG_KEY);
   if (saveMessage) {
     console.log(saveMessage);
+    textareas.value = saveMessage;
   }
 }
 onRefreshPage();
