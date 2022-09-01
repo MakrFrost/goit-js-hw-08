968dbdacddae14661e73c22d4cd6e6ca100fc034
MakrFrost <101950285+MakrFrost@users.noreply.github.com>
2022-09-01 10:46:20 +0000

    Deploying to gh-pages from @ MakrFrost/goit-js-hw-08@6aeeddd0a317ecd0c43f198f2c87e5d48c98128e 🚀

--- 03-feedback.2922d4bb.js
+++
@@ -1,4 +0,0 @@
-!function(){console.log("Это фидбек клайентса!");
-//!
-var e,t=document.querySelector(".feedback-form"),o=document.querySelector(".feedback-form textarea"),a="feedback-form-state";t.addEventListener("submit",(function(e){e.preventDefault(),console.log("Вы отправили форму вашему менеджеру!"),e.target.reset(),localStorage.removeItem(a)})),o.addEventListener("input",(function(e){var t=e.target.value;console.log(t),localStorage.setItem(a,t)})),(e=localStorage.getItem(a))&&(console.log(e),o.value=e)}();
-//# sourceMappingURL=03-feedback.2922d4bb.js.map
