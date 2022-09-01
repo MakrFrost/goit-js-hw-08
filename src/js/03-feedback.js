bd9db2ff65433182519b85e3f86c81dd5c0ad37b
MakrFrost <frostmark@ukr.net>
2022-09-01 12:20:00 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,3 +2,11 @@ function feedback() {
   console.log('Это фидбек клайентса!');
 }
 feedback();
+
+const form = document.querySelector('feedback-form');
+const email = document.getElementsByName('email')[0];
+const message = document.getElementsByName('message')[0];
+
+console.log(form);
+console.log(email);
+console.log(message);
